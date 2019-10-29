f44f5772699c900a30584c94c0116f80a94a0372
宁*依然在 <ningyiranzai@ningyirzaideMBP.lan>
2019-10-29 22:47:50 +0800

    Initial commit

---
+++ app/src/main/java/com/njy/myapplication/RxJavaActivity.java
@@ -0,0 +1,72 @@
+package com.njy.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class RxJavaActivity extends AppCompatActivity {
+    private static final String TAG = "RxJavaActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_java);
+        Observable.just("1","2").subscribe();
+    }
+
+    public void test1() {
+        Observer<String> observer = new Observer<String>() {
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.i(TAG, "onSubscribe: ");
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+
+    public void obserTestTwo() {
+        Subscriber<String> subscriber = new Subscriber<String>() {
+            @Override
+            public void onSubscribe(Subscription s) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+    }
+}
